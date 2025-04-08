4ce95e2ad97d592125d1846e2b5f359b03c52a11
Roman2508 <2508romaa@gmail.com>
2025-04-08 21:30:09 +0300

    add allowedOrigins

--- next.config.ts
+++ next.config.ts
@@ -6,40 +6,9 @@ const withNextIntl = createNextIntlPlugin("./src/libs/i18n/request.ts");
 const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
-      // edit: updated to new key. Was previously `allowedForwardedHosts`
-      allowedOrigins: [
-        // "https://pay.fondy.eu",
-        // "pay.fondy.eu",
-        // "https://7edf-2a02-2378-1284-85-7d89-eb06-d310-3600.ngrok-free.app",
-        // "7edf-2a02-2378-1284-85-7d89-eb06-d310-3600.ngrok-free.app",
-        // "http://localhost:7777",
-        // "localhost:7777",
-        // "http://localhost:3000",
-        // "localhost:3000",
-        "secure-redirect.cloudipsp.com",
-      ],
+      allowedOrigins: ["secure-redirect.cloudipsp.com", "https://pay.fondy.eu", "pay.fondy.eu"],
     },
   },
-  //   async rewrites() {
-  //     return [
-  //       {
-  //         source: '/catalog',
-  //         destination: 'https://9e68-109-251-250-156.ngrok-free.app/thank-you',
-  //       },
-  //     ]
-  //   },
-  //   async headers() {
-  //     return [
-  //       {
-  //         source: '/(.*)',
-  //         headers: [
-  //           { key: 'Access-Control-Allow-Origin', value: '*' },
-  //           { key: 'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS' },
-  //           { key: 'Access-Control-Allow-Headers', value: 'Content-Type, X-Forwarded-Host' },
-  //         ],
-  //       },
-  //     ]
-  //   },
 };
 
 export default withNextIntl(nextConfig);
---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const middleware = (request: NextRequest) => {
+  // const SESSION_NAME = process.env.NEXT_PUBLIC_SESSION_NAME ?? 'ai-shop-session'
+  // const session = request.cookies.get(SESSION_NAME)?.value
+
+  const isAuth = request.cookies.get("isAuth")?.value;
+
+  const isProfilePage = request.url.includes("/profile");
+  const isAdminPage = request.url.includes("/admin");
+
+  if (isProfilePage || isAdminPage) {
+    // if (!session) {
+    if (isAuth !== "1") {
+      return NextResponse.redirect(new URL("/catalog", request.url));
+    }
+
+    return NextResponse.next();
+  }
+};
+
+const config = {
+  matcher: ["/profile", "/profile/:path*", "/admin", "/admin/:path*", "/catalog", "/catalog/:path*"],
+};
+
+export { middleware, config };
