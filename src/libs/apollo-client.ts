3e3441832172d3105c154b5b298a437bbd2c4df0
Roman2508 <2508romaa@gmail.com>
2025-04-08 10:59:54 +0300

    apollo config3

--- src/libs/apollo-client.ts
+++ src/libs/apollo-client.ts
@@ -2,13 +2,13 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 import createUploadLink from 'apollo-upload-client/createUploadLink.mjs'
 
 export const client = new ApolloClient({
-  link: createUploadLink({
-    uri: process.env.NEXT_PUBLIC_SERVER_URL,
-    credentials: 'include',
-    headers: {
-      'apollo-require-preflight': 'true',
-    },
-  }),
+  // link: createUploadLink({
+  //   uri: process.env.NEXT_PUBLIC_SERVER_URL,
+  //   credentials: 'include',
+  //   headers: {
+  //     'apollo-require-preflight': 'true',
+  //   },
+  // }),
   uri: process.env.NEXT_PUBLIC_SERVER_URL,
   credentials: 'include',
   cache: new InMemoryCache(),
