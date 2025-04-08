599929cb83ed12750f021dfb5f8c5bafbcdeadb5
Roman2508 <2508romaa@gmail.com>
2025-04-08 21:19:37 +0300

    without middleware

--- src/app/(no-layout)/layout.tsx
+++ src/app/(no-layout)/layout.tsx
@@ -1,14 +1,8 @@
 "use client";
 
-import { Montserrat } from "next/font/google";
+// import { Montserrat } from "next/font/google";
 
 import "../../styles/global.css";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-montserrat",
-});
 
 export default function Layout({
   children,
@@ -16,8 +10,8 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning>
-      <body className={`${montserrat.variable} antialiased min-h-screen flex flex-col`}>
+    <html>
+      <body className={`antialiased min-h-screen flex flex-col`}>
         <main className="grow pt-[110px] pb-[100px]">
           <br />
           {children}
